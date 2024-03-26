de6c3019840e249fa15063f1d836ba5decdbda5a
Sugam Neupane <neupane.sugam.19.sn@gmail.com>
2024-03-26 16:46:27 +0530

    Update SharedState.js

--- frontend/src/context/SharedState.js
+++ frontend/src/context/SharedState.js
@@ -5,7 +5,7 @@ const Context = createContext();
 
 const SharedState = (props) => {
 
-    const hostname = process.env.REACT_APP_LOCALHOST
+    const hostname = process.env.REACT_APP_HOSTNAME
 
     const [loading, setLoading] = useState(false);
 
