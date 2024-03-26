5b004bc0055a525b7252d0a1b35e1045c95ba28b
Sugam Neupane <neupane.sugam.19.sn@gmail.com>
2024-03-26 16:33:19 +0530

    Update Contactus.js

--- frontend/src/components/Contactus.js
+++ frontend/src/components/Contactus.js
@@ -1,34 +1,88 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
 
-export default function Contactus() {
+export default function Contactus(props) {
+
+    const hostname = process.env.REACT_APP_PORTFOLIO_SERVER
+
+    const [userInput, setUserInput] = useState({})
+    const handleInput = (e) => {
+        const { name, value } = e.target;
+        setUserInput({
+            ...userInput,
+            [name]: value
+        })
+    }
+
+    const submitData = async (e) => {
+        e.preventDefault();
+        try {
+            await props.showAlert("Sending your message...", "danger");
+            await axios.post(hostname+'/api/sendMail', userInput);
+            await props.showAlert("Message Sent! I will respond to you probably within 24 hours!", "success");
+            await e.target.reset();
+            setUserInput({});
+        } catch (error) {
+            if(error.response.status === 429){
+                props.showAlert("Too many request! Try again later", "danger");
+                return
+            }
+            props.showAlert("Server Problem! Try again later", "danger");
+            console.log(error);
+        }
+    };
+
     return (
         <>
-                <div className='container Container bigContainer text-light p-4 appearfromTop col-sm-4 rounded'>
-                    <h1>Get in touch!</h1>
-                    <hr className='text-light mb-4'/>
-                    <form>
-                        <div className='row g-2'>
-                            <div class=" col form-floating mb-3 me-2">
-                                <input type="email" class="form-control rounded-4 border-dark" placeholder="First Name" required />
-                                <label className='text-dark'>First Name<span className='text-danger'>*</span></label>
-                            </div>
-                            <div class=" col form-floating mb-4">
-                                <input type="email" class="form-control rounded-4 border-dark" placeholder="Last Name" required />
-                                <label className='text-dark'>Last Name<span className='text-danger'>*</span></label>
-                            </div>
-                        </div>
-                        <div class="col form-floating">
-                            <input type="email" class="form-control rounded-5 border-dark" placeholder="Password" required />
-                            <label className='text-dark'>Email address<span className='text-danger'>*</span></label>
-                            <div id="emailHelp" className="form-text text-start text-light ms-3">We'll never share your email with anyone else.</div>
+                <div className='container Container bigContainer text-light p-4 appearfromTop col-sm-6 rounded'>
+                <section>
+
+                    <div className='container row m-auto'>
+                        <div className='col-sm-6 text-start border-end mb-3 mt-3' data-aos="fade-right" data-aos-duration="2000">
+                            <span className='fs-1'>Getting in touch is easy!</span>
+                            <br />
+                            <span className="material-symbols-outlined me-2 mt-3">location_on</span>
+                            <span className=''>Bengaluru, Karnataka, India</span>
+                            <br />
+                            <span className="material-symbols-outlined me-2 mt-3">call</span>
+                            <span className=''>(+91) 123 321 1221</span>
+                            <br />
+                            <span className="material-symbols-outlined me-2 mt-3">mail</span>
+                            <span className=''>contact@neupanesugam.com.np</span>
                         </div>
 
-                        <div class="form-floating mt-4">
-                            <textarea class="form-control rounded-4 border-dark" placeholder="Leave a comment here" style={{ height: 100 }} required></textarea>
-                            <label className='text-dark'>Message<span className='text-danger'>*</span></label>
+                        <div className='col-sm-6 mt-3 text-start' data-aos="fade-left" data-aos-duration="2000">
+                            <form onSubmit={submitData}>
+                                <div className="mb-3">
+                                    <label className="form-label">Your Name <span className='text-danger'>*</span></label>
+                                    <input type="text" className="form-control fs-5" name='name' placeholder='Full name' onChange={handleInput} required />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label className="form-label">Email address <span className='text-danger' style={{ fontSize: 15 }}>* must be valid</span></label>
+                                    <input type="email" className="form-control fs-5" name='email' placeholder='@' onChange={handleInput} required />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label className="form-label">Phone Number <span className='text-danger' style={{ fontSize: 14 }}>(optional)</span></label>
+                                    <input type="tel" className="form-control fs-5" name='phone' placeholder='+91-' onChange={handleInput} required />
+                                </div>
+
+                                <div className="mb-3">
+                                    <label className="form-label">Your message <span className='text-danger'>*</span></label>
+                                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="4"
+                                        placeholder='Write your message here' name='message' onChange={handleInput} required></textarea>
+                                </div>
+
+                                <div className='text-end'>
+                                    <button className={`btn btn-light`} type='submit'>Send Message</button>
+                                </div>
+                            </form>
                         </div>
-                        <button type="submit" className="btn btn-success text-center rounded-4 mt-4">Send Message</button>
-                    </form>
+
+                    </div>
+
+                </section>
                 </div>
         </>
     )
